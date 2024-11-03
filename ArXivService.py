221b1efc70d025b89f5d1caa06f82cc36a60e87a
Martin <martin.nguyen.20289@gmail.com>
2024-11-03 13:31:20 -0700

    enabled cors

--- ArXivService.py
+++ ArXivService.py
@@ -2,9 +2,9 @@ import requests
 import xml.etree.ElementTree as ElementTree
 
 class ArXivService:
-    def get_search_results(self, search_query: str, start: str, max_results: str):
-        api_url = 'http://export.arxiv.org/api/query?search_query={}&start={}&max_results={}'
-        resp = requests.get(api_url.format(search_query, start, max_results))
+    def get_search_results(self, search_query: str, start: str, max_results: str, sort_by: str, sort_order: str):
+        api_url = 'http://export.arxiv.org/api/query?search_query={}&start={}&max_results={}&sortBy={}&sortOrder={}'
+        resp = requests.get(api_url.format(search_query, start, max_results, sort_by, sort_order))
         root = ElementTree.fromstring(resp.content)
         ns = {'base_ns': 'http://www.w3.org/2005/Atom'}
         entries = []
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from ArXivService import ArXivService
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 arXivService = ArXivService()
 
 @app.route('/api/search', methods=['GET'])
+@cross_origin()
 def search():
     search_query = request.args.get('search_query')
     start = request.args.get('start')
     max_results = request.args.get('max_results')
-    search_results = arXivService.get_search_results(search_query, start, max_results)
+    sort_by = request.args.get('sort_by')
+    sort_order = request.args.get('sort_order')
+    search_results = arXivService.get_search_results(search_query, start, max_results, sort_by, sort_order)
     return jsonify(search_results)
